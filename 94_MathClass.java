17a9a76a8d495dba8c9dd2887d047a85f3c89d0c
Nikhil Parmar <nikhilparmarofc@gmail.com>
2025-12-20 20:03:52 +0530

    updated main program

---
+++ 94_MathClass.java
@@ -0,0 +1,9 @@
+import java.lang.Math;
+public class MathClass {
+    public static void main(String[] args) {
+        int a=10;
+        int b=49;
+        System.out.println("Max Number is: " + Math.max(a, b));
+        System.out.println("Square root is: " + Math.sqrt(b));
+    }
+}
