228f4cc79e713e8ef20209b21d520e2e240715e9
thebitbros <thebitbros30@gmail.com>
2026-01-01 21:09:07 +0530

    First Push of Java OOPs

---
+++ 02-oops/Class_01_Creating.java
@@ -0,0 +1,21 @@
+// WAP to Create a Normal Class
+// Date: 01/01/2026
+// Author: Nikhil
+
+class Student {
+    String name;
+    int rollNo;
+    int marks;
+}
+
+public class Class_01_Creating {
+    public static void main(String[] args) {
+        Student s1 = new Student();
+
+        s1.name = "Nikhil";
+        s1.rollNo = 18;
+        s1.marks = 93;
+
+        System.out.println(s1.name);
+    }
+}
